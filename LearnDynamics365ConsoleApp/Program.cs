ef6bc0b44b6c0b6c8f5eb7ac225315b8612cdd34
Alina <alinasoldatkina2014@gmail.com>
2020-12-28 09:38:23 +0300

    Создано консольное приложение

---
+++ LearnDynamics365ConsoleApp/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Client;
+using Microsoft.Xrm.Sdk.Messages;
+using System.Net;
+using System.ServiceModel.Description;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Tooling.Connector;
+using log4net.Repository.Hierarchy;
+using log4net;
+using log4net.Config;
+using System.Linq;
+
+namespace LearnDynamics365ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CrmServiceClient Service;
+            log4net.Config.XmlConfigurator.Configure();
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            ILog _Log = LogManager.GetLogger("LOGGER");
+
+            var connectionString = MyOptions.connectionString;
+
+            Service = new CrmServiceClient(connectionString);
+
+            if (Service == null)
+                _Log.Info("Ошибка соединения");
+
+            RetriveContact(Service, _Log);
+            Console.ReadKey();
+        }
+
+        public static void RetriveContact(IOrganizationService _service, ILog _Log)
+        {
+            // Выбираем тех у кого есть телефон1 и email1 и нет связи со средствами связи
+            string fetchXml = @"<fetch version='1.0' output-format='xml-platform' mapping='logical' distinct='true'>
+                                    <entity name='contact'>
+                                    <attribute name='fullname'/>
+                                    <attribute name='telephone1'/>
+                                    <attribute name='contactid'/>
+                                    <order attribute='fullname' descending='false'/>
+                                    <filter type='and'>
+                                        <filter type='or'>
+                                        <condition attribute='telephone1' operator='not-null'/>
+                                        <condition attribute='emailaddress2' operator='not-null'/>
+                                        </filter>
+                                    </filter>
+                                    <link-entity name='ptest_communication' from='ptest_contactid' to='contactid' link-type='outer' alias='am' />
+                                        <filter type = 'and' >
+                                        <condition entityname='am' attribute='ptest_contactid' operator='null' />
+                                        </filter > 
+                                     </entity>
+                                </fetch>";
+
+            
+            EntityCollection result = _service.RetrieveMultiple(new FetchExpression(fetchXml));
+
+            var listContacts = result.Entities;
+
+            _Log.Info($"Выполнен fetchXml запрос. Количество записей {listContacts.Count}");
+
+            foreach(var contact in listContacts)
+            {
+                string email, phone;
+                contact.TryGetAttributeValue("emailaddress1", out email);
+                contact.TryGetAttributeValue("telephone1", out phone);
+
+                _Log.Info($"Начата запись новых элементов \"Средства связи\"");
+                //объевление нового элемента для сохранения
+                Entity communication = new Entity("ptest_communication");
+                //Создание связи с контактом
+                EntityReference conctactRef = new EntityReference("contact", contact.GetAttributeValue<Guid>("contactid"));
+
+                //Заполнение полей
+                communication["ptest_contactid"] = conctactRef;
+                communication["ptest_name"] = contact["fullname"];
+
+                if (phone.Length > 0)
+                {
+                    communication["ptest_phone"] = phone;
+                    communication["ptest_type"] = new OptionSetValue(234260000);
+                    communication["ptest_main"] = true;
+                    Guid communicationId = _service.Create(communication);
+                    _Log.Info($"Создано новое средство связи с типом телефон. Id: {communicationId}");
+                }
+                if (email.Length > 0)
+                {
+                    communication["ptest_email"] = email;
+                    communication["ptest_type"] = new OptionSetValue(234260001);
+                    communication["ptest_main"] = false;
+                    Guid communicationId = _service.Create(communication);
+                    _Log.Info($"Создано новое средство связи с типом email. Id: {communicationId}");
+                }
+            }
+        }
+    }
+}
---
+++ LearnDynamics365ConsoleApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("LearnDynamics365ConsoleApp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LearnDynamics365ConsoleApp")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("67dfde03-94fd-4d90-80f3-40edf6b4c1fc")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
